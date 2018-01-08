1b1710586febc4087c7e36e209c99a420744a2d0
Kurokoda <indigo.developer@gmail.com>
2018-01-08 17:51:01 -0500

    Fixing references

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,49 @@
-const gulp = require('gulp'),
-  $ = require('gulp-load-plugins')({ camelize: true }),
-  runSequence = require('run-sequence')
+const gulp        = require('gulp'),
+      $           = require('gulp-load-plugins')({camelize: true}),
+      runSequence = require('run-sequence');
 
-gulp.task('css', function() {
+gulp.task('css', function () {
   return gulp.src('app/styles/main.scss')
-           .pipe($.sourcemaps.init())
-           .pipe($.sass().on('error', $.sass.logError))
-           .pipe($.autoprefixer({
-             browsers: ['last 2 versions'],
-             cascade: false
-           }))
-           .pipe($.sourcemaps.write())
-           .pipe(gulp.dest('./dist'))
-})
+  .pipe($.sourcemaps.init())
+  .pipe($.sass().on('error', $.sass.logError))
+  .pipe($.autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade : false
+  }))
+  .pipe($.sourcemaps.write())
+  .pipe(gulp.dest('./dist/css'))
+});
 
-gulp.task('css:watch', ['css'], function() {
+gulp.task('css:watch', ['css'], function () {
   gulp.watch('app/styles/**/*.scss', ['css'])
-})
+});
 
-gulp.task('moveAssets', function() {
+gulp.task('moveAssets', function () {
   return gulp.src('./app/assets/**/*')
-             .pipe(gulp.dest('./dist/assets'))
-})
+  .pipe(gulp.dest('./dist/assets'))
+});
 
-gulp.task('build:revAssets', ['css', 'moveAssets'], function() {
+gulp.task('build:revAssets', ['css', 'moveAssets'], function () {
   const rev = new $.revAll()
   return gulp.src('./dist/**/*')
-             .pipe(rev.revision())
-             .pipe(gulp.dest('./dist/public'))
-             .pipe(rev.manifestFile())
-             .pipe(gulp.dest('./dist'))
-})
+  .pipe(rev.revision())
+  .pipe(gulp.dest('./dist/public'))
+  .pipe(rev.manifestFile())
+  .pipe(gulp.dest('./dist'))
+});
 
-gulp.task('build:cpServer', function() {
+gulp.task('build:cpServer', function () {
   return gulp.src('./app/**/*.{js,ejs}')
-             .pipe(gulp.dest('./dist/server-build'))
-})
-gulp.task('build:revServer', ['build:cpServer'], function() {
+  .pipe(gulp.dest('./dist/server-build'))
+});
+
+gulp.task('build:revServer', ['build:cpServer'], function () {
   const manifest = gulp.src('./dist/rev-manifest.json')
   return gulp.src('./dist/server-build/{components,containers}/**/*')
-             .pipe($.revReplace({ manifest: manifest }))
-             .pipe(gulp.dest('./dist/server-build'))
-})
+  .pipe($.revReplace({manifest: manifest}))
+  .pipe(gulp.dest('./dist/server-build'))
+});
 
-gulp.task('build', function() {
+gulp.task('build', function () {
   runSequence('build:revAssets', 'build:revServer')
-})
+});
